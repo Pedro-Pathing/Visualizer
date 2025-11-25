30e1d015aec6e46237eb2c590d088ce70f836aa5
BaronClaps <126834072+BaronClaps@users.noreply.github.com>
2025-11-24 22:08:29 -0600

    bye analytics

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,6 @@
   import Navbar from "./lib/Navbar.svelte";
   import MathTools from "./lib/MathTools.svelte";
   import _ from "lodash";
-  import { Analytics } from "@vercel/analytics/next"
   import {
     easeInOutQuad,
     getCurvePoint,
@@ -139,7 +138,7 @@
         }
       });
     });
-    
+
     return _points;
   })();
 
